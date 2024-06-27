df81298d3ade156bb75ab7d04f86c26f7ea78241
lwinminoo <lwinminoo@abccsmm.com>
2024-06-27 15:35:37 +0630

    update code

--- script.ts
+++ script.ts
@@ -3,28 +3,76 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.deleteMany();
-  const user = await prisma.user.create({
-    data: {
-      name: "testing",
-      email: "testing a email",
-      age: 12,
-      UserPreference: {
-        create: {
-          emailUpdates: true,
-        },
-      },
+  // await prisma.user.deleteMany();
+  // const user = await prisma.user.create({
+  //   data: {
+  //     name: "testing4",
+  //     email: "testing4 a email",
+  //     age: 14,
+  //     UserPreference: {
+  //       create: {
+  //         emailUpdates: true,
+  //       },
+  //     },
+  //   },
+  //   // include : { UserPreference : true}
+  //   select: {
+  //     name: true,
+  //     UserPreference: {
+  //       select: {
+  //         id: true,
+  //       },
+  //     },
+  //   },
+  // });
+
+  // const user = await prisma.user.findMany({
+  //   where: {
+  //     // name: "testing",
+  //     // name: { equals:"testing"},
+  //     // name: { not:"testing"},
+  //     // name: { in: ["testing", "sally"] },
+
+  //     // age: { gt: 20 },
+  //     // email : {contains : ""}
+  //     email : {startsWith : "t"}
+  //     // AND: [
+  //     //   { email: { startsWith: "testing" } },
+  //     //   { name: { contains: "test" } },
+  //     // ],
+
+  //     // NOT: [
+  //     //   { email: { startsWith: "testing" } },
+  //     //   { name: { contains: "test" } },
+  //     // ],
+  //   },
+  //   // select: {
+  //   //   name: true,
+  //   // },
+  //   // distinct: ["name"],
+  //   take: 10,
+  //   skip: 1,
+  //   orderBy: {
+  //     age: "asc",
+  //   },
+  // });
+
+
+  // const user = await prisma.user.update(
+  //   {
+  //     where : {
+  //       id : "006e2f52-a7c4-4d8b-abe0-2e801517b010"
+  //     },
+  //     data :{
+  //       name : "testing"
+  //     }
+  //   }
+  // )
+  const user = await prisma.user.deleteMany({
+    where : {
+      age : { lt : 10}
     },
-    // include : { UserPreference : true}
-    select: {
-      name: true,
-      UserPreference: {
-        select: {
-          id: true,
-        },
-      },
-    },
-  });
+  })
   console.log(user);
 }
 
